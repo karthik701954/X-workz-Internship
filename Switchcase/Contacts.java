90395d7c37ad5adb47d3dc170660c3ad9780751d
Karthik T S <kkarthik03456@gmail.com>
2026-03-16 16:29:12 +0530

    Switchcase

---
+++ Switchcase/Contacts.java
@@ -0,0 +1,614 @@
+class Contacts{
+	
+	public static long SreachMobileNumber(String Name){
+	
+		System.out.println("The Mobile contacts Numbers are Invoking......");
+		
+		switch(Name){
+			
+			case "Manoj":
+			System.out.println("Manoj");
+			return 9012345601L;
+
+			case "Darshan":
+			System.out.println("Darshan");
+			return 9012345602L;
+
+			case "Chethan":
+			System.out.println("Chethan");
+			return 9012345603L;
+
+			case "Gowtham":
+			System.out.println("Gowtham");
+			return 9012345604L;
+
+			case "Ravi":
+			System.out.println("Ravi");
+			return 9012345605L;
+
+			case "Kiran":
+			System.out.println("Kiran");
+			return 9012345606L;
+
+			case "Suresh":
+			System.out.println("Suresh");
+			return 9012345607L;
+
+			case "Mahesh":
+			System.out.println("Mahesh");
+			return 9012345608L;
+
+			case "Naveen":
+			System.out.println("Naveen");
+			return 9012345609L;
+
+			case "Rakesh":
+			System.out.println("Rakesh");
+			return 9012345610L;
+
+			case "Vinay":
+			System.out.println("Vinay");
+			return 9012345611L;
+
+			case "Pradeep":
+			System.out.println("Pradeep");
+			return 9012345612L;
+
+			case "Lokesh":
+			System.out.println("Lokesh");
+			return 9012345613L;
+
+			case "Santosh":
+			System.out.println("Santosh");
+			return 9012345614L;
+
+			case "Deepak":
+			System.out.println("Deepak");
+			return 9012345615L;
+
+			case "Harish":
+			System.out.println("Harish");
+			return 9012345616L;
+
+			case "Anil":
+			System.out.println("Anil");
+			return 9012345617L;
+
+			case "Sunil":
+			System.out.println("Sunil");
+			return 9012345618L;
+
+			case "Manjunath":
+			System.out.println("Manjunath");
+			return 9012345619L;
+
+			case "Prakash":
+			System.out.println("Prakash");
+			return 9012345620L;
+
+			case "Ajay":
+			System.out.println("Ajay");
+			return 9012345621L;
+
+			case "Arun":
+			System.out.println("Arun");
+			return 9012345622L;
+
+			case "Bharath":
+			System.out.println("Bharath");
+			return 9012345623L;
+
+			case "Chandru":
+			System.out.println("Chandru");
+			return 9012345624L;
+
+			case "Dinesh":
+			System.out.println("Dinesh");
+			return 9012345625L;
+
+			case "Eshwar":
+			System.out.println("Eshwar");
+			return 9012345626L;
+
+			case "Farhan":
+			System.out.println("Farhan");
+			return 9012345627L;
+
+			case "Ganesh":
+			System.out.println("Ganesh");
+			return 9012345628L;
+
+			case "Hemanth":
+			System.out.println("Hemanth");
+			return 9012345629L;
+
+			case "Imran":
+			System.out.println("Imran");
+			return 9012345630L;
+			
+						case "Jagdish":
+			System.out.println("Jagdish");
+			return 9012345631L;
+
+			case "Karthik":
+			System.out.println("Karthik");
+			return 9012345632L;
+
+			case "Lohith":
+			System.out.println("Lohith");
+			return 9012345633L;
+
+			case "Madan":
+			System.out.println("Madan");
+			return 9012345634L;
+
+			case "Nithin":
+			System.out.println("Nithin");
+			return 9012345635L;
+
+			case "Omkar":
+			System.out.println("Omkar");
+			return 9012345636L;
+
+			case "Pavan":
+			System.out.println("Pavan");
+			return 9012345637L;
+
+			case "Qasim":
+			System.out.println("Qasim");
+			return 9012345638L;
+
+			case "Raghu":
+			System.out.println("Raghu");
+			return 9012345639L;
+
+			case "Shankar":
+			System.out.println("Shankar");
+			return 9012345640L;
+
+			case "Tarun":
+			System.out.println("Tarun");
+			return 9012345641L;
+
+			case "Umesh":
+			System.out.println("Umesh");
+			return 9012345642L;
+
+			case "Vikas":
+			System.out.println("Vikas");
+			return 9012345643L;
+
+			case "Wasim":
+			System.out.println("Wasim");
+			return 9012345644L;
+
+			case "Xavier":
+			System.out.println("Xavier");
+			return 9012345645L;
+
+			case "Yogesh":
+			System.out.println("Yogesh");
+			return 9012345646L;
+
+			case "Zubair":
+			System.out.println("Zubair");
+			return 9012345647L;
+
+			case "Abhishek":
+			System.out.println("Abhishek");
+			return 9012345648L;
+
+			case "Balaji":
+			System.out.println("Balaji");
+			return 9012345649L;
+
+			case "Chirag":
+			System.out.println("Chirag");
+			return 9012345650L;
+
+			case "Dev":
+			System.out.println("Dev");
+			return 9012345651L;
+
+			case "Eshan":
+			System.out.println("Eshan");
+			return 9012345652L;
+
+			case "Faiz":
+			System.out.println("Faiz");
+			return 9012345653L;
+
+			case "Girish":
+			System.out.println("Girish");
+			return 9012345654L;
+
+			case "Harsha":
+			System.out.println("Harsha");
+			return 9012345655L;
+
+			case "Irfan":
+			System.out.println("Irfan");
+			return 9012345656L;
+
+			case "Jayanth":
+			System.out.println("Jayanth");
+			return 9012345657L;
+
+			case "Kishore":
+			System.out.println("Kishore");
+			return 9012345658L;
+
+			case "Lakshman":
+			System.out.println("Lakshman");
+			return 9012345659L;
+
+			case "Mithun":
+			System.out.println("Mithun");
+			return 9012345660L;
+			
+						case "Naveed":
+			System.out.println("Naveed");
+			return 9012345661L;
+
+			case "Owais":
+			System.out.println("Owais");
+			return 9012345662L;
+
+			case "Pranav":
+			System.out.println("Pranav");
+			return 9012345663L;
+
+			case "Rohit":
+			System.out.println("Rohit");
+			return 9012345664L;
+
+			case "Sandeep":
+			System.out.println("Sandeep");
+			return 9012345665L;
+
+			case "Tejas":
+			System.out.println("Tejas");
+			return 9012345666L;
+
+			case "Uday":
+			System.out.println("Uday");
+			return 9012345667L;
+
+			case "Varun":
+			System.out.println("Varun");
+			return 9012345668L;
+
+			case "Waseem":
+			System.out.println("Waseem");
+			return 9012345669L;
+
+			case "Xander":
+			System.out.println("Xander");
+			return 9012345670L;
+
+			case "Yash":
+			System.out.println("Yash");
+			return 9012345671L;
+
+			case "Zaid":
+			System.out.println("Zaid");
+			return 9012345672L;
+
+			case "Akash":
+			System.out.println("Akash");
+			return 9012345673L;
+
+			case "Bhavesh":
+			System.out.println("Bhavesh");
+			return 9012345674L;
+
+			case "Charan":
+			System.out.println("Charan");
+			return 9012345675L;
+
+			case "Dharma":
+			System.out.println("Dharma");
+			return 9012345676L;
+
+			case "Eshaan":
+			System.out.println("Eshaan");
+			return 9012345677L;
+
+			case "Faisal":
+			System.out.println("Faisal");
+			return 9012345678L;
+
+			case "Gagan":
+			System.out.println("Gagan");
+			return 9012345679L;
+
+			case "Hari":
+			System.out.println("Hari");
+			return 9012345680L;
+
+			case "Indrajit":
+			System.out.println("Indrajit");
+			return 9012345681L;
+
+			case "Jeevan":
+			System.out.println("Jeevan");
+			return 9012345682L;
+
+			case "Kunal":
+			System.out.println("Kunal");
+			return 9012345683L;
+
+			case "Lalit":
+			System.out.println("Lalit");
+			return 9012345684L;
+
+			case "Manish":
+			System.out.println("Manish");
+			return 9012345685L;
+
+			case "Nakul":
+			System.out.println("Nakul");
+			return 9012345686L;
+
+			case "Om":
+			System.out.println("Om");
+			return 9012345687L;
+
+			case "Pritam":
+			System.out.println("Pritam");
+			return 9012345688L;
+
+			case "Rajat":
+			System.out.println("Rajat");
+			return 9012345689L;
+
+			case "Siddharth":
+			System.out.println("Siddharth");
+			return 9012345690L;
+			
+						case "Tanmay":
+			System.out.println("Tanmay");
+			return 9012345691L;
+
+			case "Uttam":
+			System.out.println("Uttam");
+			return 9012345692L;
+
+			case "Vishal":
+			System.out.println("Vishal");
+			return 9012345693L;
+
+			case "Wajid":
+			System.out.println("Wajid");
+			return 9012345694L;
+
+			case "Xeno":
+			System.out.println("Xeno");
+			return 9012345695L;
+
+			case "Yuvraj":
+			System.out.println("Yuvraj");
+			return 9012345696L;
+
+			case "Zeeshan":
+			System.out.println("Zeeshan");
+			return 9012345697L;
+
+			case "Aditya":
+			System.out.println("Aditya");
+			return 9012345698L;
+
+			case "Bhdra":
+			System.out.println("Bhdra");
+			return 9012345699L;
+
+			case "Chaitanya":
+			System.out.println("Chaitanya");
+			return 9012345700L;
+
+			case "Danish":
+			System.out.println("Danish");
+			return 9012345701L;
+
+			case "Ebrahim":
+			System.out.println("Ebrahim");
+			return 9012345702L;
+
+			case "Farooq":
+			System.out.println("Farooq");
+			return 9012345703L;
+
+			case "Gokul":
+			System.out.println("Gokul");
+			return 9012345704L;
+
+			case "Hitesh":
+			System.out.println("Hitesh");
+			return 9012345705L;
+
+			case "Ishaan":
+			System.out.println("Ishaan");
+			return 9012345706L;
+
+			case "Jitendra":
+			System.out.println("Jitendra");
+			return 9012345707L;
+
+			case "Karna":
+			System.out.println("Karna");
+			return 9012345708L;
+
+			case "Lokendra":
+			System.out.println("Lokendra");
+			return 9012345709L;
+
+			case "Mohit":
+			System.out.println("Mohit");
+			return 9012345710L;
+
+			case "Nagesh":
+			System.out.println("Nagesh");
+			return 9012345711L;
+
+			case "Ojas":
+			System.out.println("Ojas");
+			return 9012345712L;
+
+			case "Prithvi":
+			System.out.println("Prithvi");
+			return 9012345713L;
+
+			case "Rudra":
+			System.out.println("Rudra");
+			return 9012345714L;
+
+			case "Sahil":
+			System.out.println("Sahil");
+			return 9012345715L;
+
+			case "Tushar":
+			System.out.println("Tushar");
+			return 9012345716L;
+
+			case "Ujjwal":
+			System.out.println("Ujjwal");
+			return 9012345717L;
+
+			case "Vignesh":
+			System.out.println("Vignesh");
+			return 9012345718L;
+
+			case "Wasif":
+			System.out.println("Wasif");
+			return 9012345719L;
+
+			case "Xavier T K":
+			System.out.println("Xavier T K");
+			return 9012345720L;
+			
+			case "Yatin":
+			System.out.println("Yatin");
+			return 9012345721L;
+
+			case "Zubair Y":
+			System.out.println("Zubair Y");
+			return 9012345722L;
+
+			case "Arjun":
+			System.out.println("Arjun");
+			return 9012345723L;
+
+			case "Bhuvan":
+			System.out.println("Bhuvan");
+			return 9012345724L;
+
+			case "Chetan":
+			System.out.println("Chetan");
+			return 9012345725L;
+
+			case "Dheeraj":
+			System.out.println("Dheeraj");
+			return 9012345726L;
+
+			case "Eshwar V":
+			System.out.println("Eshwar V");
+			return 9012345727L;
+
+			case "Faizan":
+			System.out.println("Faizan");
+			return 9012345728L;
+
+			case "Girish R":
+			System.out.println("Girish R");
+			return 9012345729L;
+
+			case "Hemanth V":
+			System.out.println("Hemanth V");
+			return 9012345730L;
+
+			case "Ibrahim":
+			System.out.println("Ibrahim");
+			return 9012345731L;
+
+			case "Jagan":
+			System.out.println("Jagan");
+			return 9012345732L;
+
+			case "Kishan":
+			System.out.println("Kishan");
+			return 9012345733L;
+
+			case "Lohit":
+			System.out.println("Lohit");
+			return 9012345734L;
+
+			case "Manjunath R":
+			System.out.println("Manjunath R");
+			return 9012345735L;
+
+			case "Nikhil":
+			System.out.println("Nikhil");
+			return 9012345736L;
+
+			case "Omkar v":
+			System.out.println("Omkar V");
+			return 9012345737L;
+
+			case "Pankaj":
+			System.out.println("Pankaj");
+			return 9012345738L;
+
+			case "Ramesh":
+			System.out.println("Ramesh");
+			return 9012345739L;
+
+			case "Shivu":
+			System.out.println("Shivu");
+			return 9012345740L;
+
+			case "Tej":
+			System.out.println("Tej");
+			return 9012345741L;
+
+			case "Uday T":
+			System.out.println("Uday T");
+			return 9012345742L;
+
+			case "Vijay":
+			System.out.println("Vijay");
+			return 9012345743L;
+
+			case "Wahid":
+			System.out.println("Wahid");
+			return 9012345744L;
+
+			case "Xeno B":
+			System.out.println("Xeno B");
+			return 9012345745L;
+
+			case "Yogendra":
+			System.out.println("Yogendra");
+			return 9012345746L;
+
+			case "Zakir":
+			System.out.println("Zakir");
+			return 9012345747L;
+
+			case "Aman":
+			System.out.println("Aman");
+			return 9012345748L;
+
+			case "Bhaskar":
+			System.out.println("Bhaskar");
+			return 9012345749L;
+
+			case "Chandu":
+			System.out.println("Chandu");
+			return 9012345750L;
+
+			default:
+			System.out.println("The Invalid Mobile Number...are:");
+			return 0L;
+		}
+	}
+}
